0c692cba4e07640f3ca0f104303d38c0d2ad1fdc
agvaibhav95 <agvaibhav95@gmail.com>
2020-09-26 20:32:43 +0530

    Update MyHashMap.java
    rehashed version

--- MyHashMap.java
+++ MyHashMap.java
@@ -1,90 +1,107 @@
-package demo;
-
 import java.util.ArrayList;
-import java.util.List;
 
-public class MyHashMap {
+public class HashMap<K,V> {
+private int capacity=16;
+private int size = 0;
+private ArrayList<Nodes<K,V>>  arr=new ArrayList<>(capacity);
+private float loadfactor=0.75f;
+private int rehashing=(int)(capacity*loadfactor);
+private int triggerrehasing=0;
 
-	class Container{
-		Object key;
-		Object value;
-		public void insert(Object k, Object v){
-			this.key=k;
-			this.value=v;
-		}
-	}
-	
-	private Container c;
-	private List<Container> recordList;
-	
-	public MyHashMap(){
+
+
+public int hashing(K key) {
+	return key.hashCode() %capacity;
+}
+
+public int size() {
+	return this.size;
+}
+public boolean isEmpty() {
+	return this.size == 0 ? true : false;
+}
+
+public boolean containsKey(K key) {
+	if(key == null) {return arr.get(0).key == null ?true:false;}
+	int index=hashing(key);
+	if(arr.get(index) != null) {
 		
-		this.recordList=new ArrayList<Container>();
-	}
-	
-	public void put(Object k, Object v){
-		this.c=new Container();
-		c.insert(k, v);
-		boolean flag =true;
-		//check for the same key before adding
-		for(int i=0; i<recordList.size(); i++){
-			Container c1=recordList.get(i);
-			if(c1.key.equals(k)){
-				//remove the existing object
-				//recordList.remove(i);
-				flag = false;
-			/*	List<Object> list = new ArrayList<>();
-				list.add(c1.value);
-				list.add(v);*/
-				break;//for list
-				//c.insert(k, list);// for list
-			}
+		if(arr.get(index).key == key) {return true;}
+		Nodes nexts=arr.get(index).next;
+		while(nexts != null) {
+			if(nexts.key == key) {return true;}
+			nexts=nexts.next;
 		}
-		if(flag)
-		recordList.add(c);
 	}
+	return false;
 	
-	public Object get(Object k){
-		for(int i=0; i<this.recordList.size(); i++){
-			Container con = recordList.get(i);
-			//System.out.println("k.toString():"+k.toString()+"con.key.toString()"+con.key.toString());
-			if (k.toString()==con.key.toString()) {
+	
+	
+	
+}
+public void rehash() {capacity*=2;
+triggerrehasing=0;
+ArrayList<Nodes<K,V>>  temp=arr;
+arr=new ArrayList<Nodes<K,V>>(capacity);
+for(Nodes<K,V> vals:temp) {
+	while(vals!=null) {
+		put(vals.key, vals.value);
+		vals=vals.next;
+	}
+}
+}
+
+public void put(K key,V Value) {
+	Nodes<K,V> node=new Nodes<K, V>(); 
+	if(key == null) {
+		if(arr.get(0).key == null) {
+			arr.add(0,node);
+		}
+	}
+	else {
+		
+		int index=hashing(key);
+		if(arr.get(index) == null) {
+			arr.add(index,node);
+			return;
+		}
+		else {
+			Nodes exist=arr.get(index);
+			while(exist!=null) {
+				if(exist.key==key) {
+					System.out.println("replaec with new value");
+					exist.value=Value;
+				}
+				exist=exist.next;
+			}
+			exist=node;
+		}
+		
+		
+	}
+	size++;
+	triggerrehasing++;
+	if(triggerrehasing>=rehashing) {
+		rehash();
+	}
+}
+public Nodes get(K key) {
+	if(key == null) {
+		return arr.get(0);
+	}
+	else {
+		int index=hashing(key);
+		if(arr.get(index).key==key) {return arr.get(index);}
+		else {
+			Nodes node=arr.get(index).next;
+			while(node!=null && node.key!=key) {
+				node=node.next;
 				
-				return con.value;
 			}
+			return node;
 			
 		}
-		return null;
 	}
 	
-	public Object print(){
-		for(int i=0; i<this.recordList.size(); i++){
-			Container con = recordList.get(i);
-			//System.out.println("k.toString():"+k.toString()+"con.key.toString()"+con.key.toString());
-			/*
-			 * if (k.toString()==con.key.toString()) {
-			 * 
-			 * return con.value; }
-			 */
-			System.out.println(con.key.toString()+" "+con.value);
-			
-		}
-		return null;
-	}
-	
-	public static void main(String[] args) {
-		MyHashMap hm = new MyHashMap();
-		hm.put("1", "1");
-		hm.put("2", "2");
-		hm.put("3", "3");
-		//System.out.println(hm.get("3"));
-		hm.put("3", "4");
-		
-		//System.out.println(hm.get("1"));
-		//System.out.println(hm.get("3"));
-		//System.out.println(hm.get("8"));
-		
-		System.out.println(hm.print());
-	}
-
+}
 }
